aba3163a2838ec356fb67813aaa0aa85ebb0ab8a
najd <najdbina@gmail.com>
2026-01-27 17:24:50 +0300

    Initial Commit

---
+++ levelUp_startupApp/levelUp_startupAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  levelUp_startupAppApp.swift
+//  levelUp_startupApp
+//
+//  Created by najd aljarba on 27/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct levelUp_startupAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ levelUp_startupAppTests/levelUp_startupAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  levelUp_startupAppTests.swift
+//  levelUp_startupAppTests
+//
+//  Created by najd aljarba on 27/01/2026.
+//
+
+import Testing
+@testable import levelUp_startupApp
+
+struct levelUp_startupAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
